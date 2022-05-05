77eaa4dc4e9c0c5c5d197457f103de51973e63cf
juliocsantaman <juliocsantaman@gmail.com>
2022-05-05 05:07:52 -0500

    API REST C# - Hello, World.

---
+++ SP_Backend/Controllers/DictionaryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SP_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DictionaryController : Controller
+    {
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "Software Project - Hello, World!";
+        }
+    }
+}
